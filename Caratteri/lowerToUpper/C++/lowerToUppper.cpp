adfc3741f2c108136491b9f4005fc287f2b96800
prof-gentile-i <127745745+prof-gentile-i@users.noreply.github.com>
2023-03-13 21:18:22 +0100

    Update lowerToUppper.cpp

--- Caratteri/lowerToUpper/C++/lowerToUppper.cpp
+++ Caratteri/lowerToUpper/C++/lowerToUppper.cpp
@@ -1,1 +1,18 @@
+// Trasforma un testo da minuscolo a maiuscolo
+// Il testo è su file
+
 #include <iostream>
+#include <fstream>
+
+int main () {
+    int c;
+
+    std::ifstream in("input.txt");
+    std::ofstream out ("output.txt");
+
+    while((c = in.get()) != EOF) {
+        if (c >= 'a' and c <='z') 
+            c = 'A' + c - 'a';
+        out << static_cast<char>(c);  // oppure (char) c    
+    }
+}
